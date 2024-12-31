d2af6927900d17286b46fcd22ad6f008d47ff454
Kaasklontje <84289148+Kaasklontje@users.noreply.github.com>
2025-01-01 00:30:25 +0100

    Update server.lua

--- src/server/server.lua
+++ src/server/server.lua
@@ -1,6 +1,17 @@
 local Config = require 'config.shared_config'
 local ServerConfig = require 'config.server_config'
 
+local function sendDiscordLog(message, color)
+    local embed = {
+        {
+            ["color"] = color,
+            ["description"] = message,
+        }
+    }
+
+    PerformHttpRequest(ServerConfig.DiscordWebhook, function(err, text, headers) end, 'POST', json.encode({username = "Server Logs", embeds = embed}), {['Content-Type'] = 'application/json'})
+end
+
 lib.addCommand(Config.Command, {
     help = locale('command_help'),
     restricted = Config.AllowedGroups,
@@ -9,15 +20,20 @@ lib.addCommand(Config.Command, {
     state.isOnDuty = not state.isOnDuty
 
     if state.isOnDuty then
+        sendDiscordLog(("**%s** is nu in dienst."):format(GetPlayerName(source)), 3066993) 
         TriggerClientEvent('illenium-appearance:client:loadJobOutfit', source, { outfitData = Config.Outfits[GetEntityModel(GetPlayerPed(source))] })
+    else
+        sendDiscordLog(("**%s** is nu uit dienst."):format(GetPlayerName(source)), 15158332) 
     end
 end)
 
 RegisterNetEvent('txsv:checkIfAdmin', function()
     local src = source
-    Wait(100) -- Required for txAdmin Wait server side.
+    Wait(100)
 
     if not Player(src).state.isOnDuty then
         TriggerClientEvent('txcl:setAdmin', src, false, false, locale('no_access'))
     end
 end)
+
+
